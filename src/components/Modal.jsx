4aece631e7891a3bf5398e3c20a871e63c57b293
NCameloG <dev.ncamelog@gmail.com>
2024-06-18 15:00:42 -0500

    "esc" closes modal added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Modal = ({ imageUrl, onClose }) => {
+  const handleKeyDown = event => {
+    if (event.keyCode === 27) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
   return (
     <div className="overlay" onClick={onClose}>
       <div className="modal" onClick={e => e.stopPropagation()}>
